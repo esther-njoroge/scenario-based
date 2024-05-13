34e2015cfb0865da39224d5f5bb573c055e9d277
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-05-13 23:05:15 +0300

    commit changes

--- prac.js
+++ prac.js
@@ -22,3 +22,42 @@ app.addFollowers("user2", "user1");
 console.log(app.gainFollowers("user1"));  
 console.log(app.gainFollowers("user2"));  
 console.log(app.gainFollowers("user3"));  
+
+
+
+// Develop a system for scheduling appointments at a healthcare facility. Consider factors like doctor availability, patient preferences, and appointment reminders.
+class scheduleAppointment {
+    constructor(doctorsDetails,patientsDetails,appointmentDate) {
+        this.doctorsDetails=[];
+        this.patientsDetails =[];
+         this.appointmentDate= appointmentDate;
+       }
+       addPatientDetails(name, age, condition, preferences){
+        const patientDetails= {
+            name: name,
+            age: age,
+            condition: condition,
+            preferences: preferences
+        }
+        this.patientsDetails.push(patientDetails)
+       }
+       addDoctors(name, skills, available=true){
+        const doctordetails = {
+            name: name,
+            skills: skills
+        }
+        this.doctorsDetails.push(doctordetails)
+        this.doctorsDetails.available.forEach(item => {
+            if (doctorsDetails.available == true){
+                console.log(`${patientDetails.name} is to consult ${doctorDetails.name} on ${this.appointmentDate}`)
+            }
+            else{
+                console.log("Doctor is not available")
+            }
+        });
+       }
+    }
+    const schedule = new Appointment()
+    schedule.addPatientDetails("David joe",23, "Breathing difficulties", )
+    schedule.addDoctors("Doctor Agnes", "General Doctor")
+    console.log(schedule)
